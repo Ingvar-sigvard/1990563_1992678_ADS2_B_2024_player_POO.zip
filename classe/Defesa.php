ad11ec5945d33621b7251d0e3db68ad385b9897c
Ingvar-sigvard <ingvartop@gmail.com>
2024-11-27 19:50:19 -0300

    Defesa.php

--- classe/Defesa.php
+++ classe/Defesa.php
@@ -1,7 +1,10 @@
 <?php
 
+require_once('Item.pgp')
+require_once('Inventario.php')
+    
 Class Defesa extends Item {
-    public function __construct($nome) {
-        parent::__construct($nome, 4);
+    public function __construct(string $nome, int $tamanho, string $classe) {
+        parent::__construct($nome, $tamanho, $classe);
     }
 }
