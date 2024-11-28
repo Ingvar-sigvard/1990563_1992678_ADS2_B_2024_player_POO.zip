6a816c1cba5177efe0d68eb720144b9c39bb6134
Ingvar-sigvard <ingvartop@gmail.com>
2024-11-27 22:18:18 -0300

    Magia.php

--- classe/Magia.php
+++ classe/Magia.php
@@ -1,7 +1,7 @@
 <?php
 
 class Magia extends Item {
-    public function __construct(string $nick, string $classe) {
+    public function __construct(string $nome, string $classe) {
         parent::__construct($nome, 2 , "magia");
     }
 }
