309ac156ec655b96cca7609ed6a6f689e296a1ed
Ingvar-sigvard <ingvartop@gmail.com>
2024-11-27 22:18:38 -0300

    Defesa.php

--- classe/Defesa.php
+++ classe/Defesa.php
@@ -1,7 +1,7 @@
 <?php
 
 class Defesa extends Item {
-    public function __construct(string $nick, string $classe) {
+    public function __construct(string $nome, string $classe) {
         parent::__construct($nome, 4, "Defesa");
     }
 }
