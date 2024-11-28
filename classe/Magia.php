a3e52e3358305ea9a54a023ba08478348cdf0dea
Ingvar-sigvard <ingvartop@gmail.com>
2024-11-27 22:15:07 -0300

    Magia.php

--- classe/Magia.php
+++ classe/Magia.php
@@ -1,10 +1,7 @@
 <?php
 
-require_once('Item.php')
-require_once('Inventario.php')    
-
 class Magia extends Item {
-    public function __construct(string $nick,int $tamanho, string$classe) {
-        parent::__construct($nome,$tamanho,$classe);
+    public function __construct(string $nick, string $classe) {
+        parent::__construct($nome, 2 , "magia");
     }
 }
