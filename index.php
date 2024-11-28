95171c047e01b57afa7e04ef0bee3a5c9d6c321f
Ingvar-sigvard <ingvartop@gmail.com>
2024-11-27 22:34:37 -0300

    index.php

--- index.php
+++ index.php
@@ -1,39 +1,32 @@
 <?php
+require_once 'class/Player.php';
+require_once 'class/Inventario.php';
+require_once 'class/Itens/Ataque.php';
+require_once 'class/Itens/Defesa.php';
+require_once 'class/Itens/Magia.php';
 
-require_once('Item.php');
-require_once('Inventario.php');
-require_once('Player.php');
-require_once('Magia.php');
-require_once('Defesa.php');
-require_once('Ataque.php');
+$player1 = new Player("Jogador 1", 1);
+$player2 = new Player("Jogador 2", 1);
 
-$player = new Player("Jogador1", 5);
+$ataque1 = new Ataque("Katana");
+$ataque2 = new Ataque("Alabarda");
+$defesa1 = new Defesa("Desviar");
+$defesa2 = new Defesa("Armadura");
+$magia1 = new Magia("Bola de Fogo");
+$magia2 = new Magia("Bola de água");
 
-echo "Nick: " . $player->getNick() . "<br>";
-echo "Nível: " . $player->getNivel() . "<br>";
+$player1->coletarItem($ataque1);
+$player1->coletarItem($defesa1);
+$player2->coletarItem($ataque2);
+$player2->coletarItem($magia1);
 
-$espada = new Ataque("Espada Longa", 5, "Ataque");
-$escudo = new Defesa("Escudo de Ferro");
-$feitiço = new Magia("Bola de Fogo", 3, "Mágico");
+echo $player1->getNickname() . " - Itens no inventário após subida de nível: </br>";
+$player1->listarItens();
 
-echo $player->getInventario()->adicionarItem($espada) . "<br>";
-echo $player->getInventario()->adicionarItem($escudo) . "<br>";
-echo $player->getInventario()->adicionarItem($feitiço) . "<br>";
 
-echo "Itens no inventário:<br>";
-foreach ($player->getInventario()->listarItens() as $item) {
-    echo "- " . $item->getNome() . " (Classe: " . $item->getClasse() . ")<br>";
-}
+echo $player2->getNickname() . " - Itens no inventário após subida de nível: </br>";
+$player2->listarItens();
 
-echo $player->soltarItem($espada) ? "Item removido com sucesso.<br>" : "Falha ao remover o item.<br>";
+$player1->subirNivel();
 
-$player->elevarNivel();
-echo "Nível elevado: " . $player->getNivel() . "<br>";
-echo "Capacidade do inventário: " . $player->getInventario()->getCapacidade() . "<br>";
-
-echo $player->esvaziarInventario() . "<br>";
-
-echo "Itens no inventário após esvaziar:<br>";
-foreach ($player->getInventario()->listarItens() as $item) {
-    echo "- " . $item->getNome() . "<br>";
-}
+<?
